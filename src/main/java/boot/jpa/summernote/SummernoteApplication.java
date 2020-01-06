0389e83062d405b73aeed79ef0fe7de1a61c5f47
KIM HONGIL <clroyan01@gmail.com>
2020-01-06 22:02:51 +0900

    Create summernote project

---
+++ src/main/java/boot/jpa/summernote/SummernoteApplication.java
@@ -0,0 +1,13 @@
+package boot.jpa.summernote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummernoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SummernoteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/boot/jpa/summernote/SummernoteApplicationTests.java
@@ -0,0 +1,13 @@
+package boot.jpa.summernote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SummernoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
