f6fdd04c4883002e5802a9914aa117b2fac3e414
KIM HONGIL <clroyan01@gmail.com>
2020-01-07 19:51:11 +0900

    Import WYSIWYG editor - summernote

---
+++ src/main/java/boot/jpa/summernote/web/WebController.java
@@ -0,0 +1,13 @@
+package boot.jpa.summernote.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
---
+++ src/main/resources/static/js/index.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $(function(){
+        $('#summernote').summernote({
+            height: 600,
+        });
+
+    });
+});
